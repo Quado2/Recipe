be93df4a1f0c5a597f91d27d2d100ecee12a2792
Quado2 <engchikwas404@gmail.com>
2022-05-19 01:32:38 +0100

    implemented stuffs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  currentPage = 'recipe';
   title = 'Recipe';
+
+  onNavigate(feature: string) {
+    this.currentPage = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: "app-header",
-  templateUrl:"./header.component.html",
-  styleUrls:["./header.component.scss"]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent{
+export class HeaderComponent {
   collapsed = true;
+  @Output() featureEmit = new EventEmitter<string>();
 
+  onSelect(feature: string) {
+    this.featureEmit.emit(feature);
+  }
 }
